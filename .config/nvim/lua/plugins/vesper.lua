9ad9c4ca3d0fd54040c101d8296fdad7ed6d8117
ishansing <ishan.nva.2005@gmail.com>
2025-05-02 18:49:22 +0000

    wsl config 02-05-25

---
+++ .config/nvim/lua/plugins/vesper.lua
@@ -0,0 +1,12 @@
+return {
+  -- add vesper
+  { "ishansing/vesper.nvim" },
+
+  -- Configure LazyVim to load vesper
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vesper",
+    },
+  },
+}
